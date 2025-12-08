2e938f7338ba8ff642a8a8bed47aae53fb203bff
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-08 15:35:13 -0400

    Agregar campos a la migración de la tabla servicios_medicos en el sistema ZoofiPets

--- System/database/migrations/2025_12_08_193244_create_servicios_medicos_table.php
+++ System/database/migrations/2025_12_08_193244_create_servicios_medicos_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('servicios_medicos', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre', 150);
+            $table->text('descripcion')->nullable();
+            $table->decimal('precio', 10, 2);
+            $table->integer('duracion_estimada_minutos')->nullable();
+            $table->enum('categoria', ['Consulta', 'Vacunacion', 'Cirugia', 'Tratamiento', 'Emergencia', 'Estetica', 'Diagnostico']);
+            $table->text('requisitos_previos')->nullable();
+            $table->boolean('requiere_cita')->default(true);
+            $table->boolean('activo')->default(true);
             $table->timestamps();
         });
     }
