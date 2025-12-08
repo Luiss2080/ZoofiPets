f4d8ec99b367bd56c5f7201d485630424332fb46
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-08 14:14:16 -0400

    Eliminar archivos .gitignore innecesarios y agregar nuevos para las carpetas de caché, logs, privado y pruebas en el sistema ZoofiPets

--- System/app/Traits/Auditable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Traits;
-
-trait Auditable
-{
-    // Trait para auditoría de cambios en el sistema veterinario ZoofiPets
-    // Se puede implementar para rastrear cambios en registros médicos, etc.
-}
