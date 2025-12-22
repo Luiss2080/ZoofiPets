b070e11722eb65b0708b8217f947f6dd16045e03
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-22 17:47:07 -0400

    feat: add HistorialMedico model with fillable attributes, date casts, and relationships.

--- System/app/Models/HistorialMedico.php
+++ System/app/Models/HistorialMedico.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class HistorialMedico extends Model
 {
+    use HasFactory;
     protected $table = 'historiales_medicos';
     
     protected $fillable = [
