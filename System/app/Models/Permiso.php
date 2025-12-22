19d546f90243f29293e8319b4f86ee808e16e897
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-22 18:03:22 -0400

    feat: Create new Eloquent models for various application entities.

--- System/app/Models/Permiso.php
+++ System/app/Models/Permiso.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Permiso extends Model
 {
+    use HasFactory;
     protected $fillable = ['nombre', 'clave', 'descripcion'];
 
     public function roles()
