9106f8069dfe3341d1f5e010c6b3ded0e33fd193
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-22 17:54:38 -0400

    feat: Add `CategoriaProducto` model and comprehensive database seeding for multiple entities.

--- System/app/Models/CategoriaProducto.php
+++ System/app/Models/CategoriaProducto.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CategoriaProducto extends Model
 {
+    use HasFactory;
     protected $table = 'categorias_productos';
     protected $fillable = ['nombre', 'descripcion', 'activo'];
 
