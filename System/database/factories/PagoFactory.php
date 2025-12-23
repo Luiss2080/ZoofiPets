83ec72779c95b4ceb6d75cfd280206a6ca6c4119
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-22 20:10:10 -0400

    feat: add PagoFactory for generating fake payment data

--- System/database/factories/PagoFactory.php
+++ System/database/factories/PagoFactory.php
@@ -10,9 +10,9 @@ class PagoFactory extends Factory
     {
         return [
             'venta_id' => \App\Models\Venta::factory(),
-            'metodo_pago_id' => \App\Models\MetodoPago::inRandomOrder()->first()?->id ?? \App\Models\MetodoPago::factory(),
-            'monto' => $this->faker->randomFloat(2, 10, 100),
-            'moneda' => 'USD',
+            'metodo_pago_id' => \App\Models\MetodoPago::factory(),
+            'monto' => $this->faker->randomFloat(2, 50, 500),
+            'fecha_pago' => $this->faker->dateTimeBetween('-1 month', 'now'),
             'tipo_comprobante' => 'Ticket',
         ];
     }
