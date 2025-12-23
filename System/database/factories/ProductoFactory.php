450868fe3d5d82bb05ad0b18ba233a5b7ee8b47a
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-22 20:41:33 -0400

    feat: add `Producto` model factory and a debug script for creating `Pago` instances.

--- System/database/factories/ProductoFactory.php
+++ System/database/factories/ProductoFactory.php
@@ -20,7 +20,21 @@ class ProductoFactory extends Factory
         return [
             'codigo_interno' => $this->faker->unique()->bothify('PROD-####'),
             'codigo_barras' => $this->faker->ean13(),
-            'nombre' => $this->faker->words(3, true),
+            'nombre' => $this->faker->randomElement([
+                'Alimento Premium Perro Adulto 15kg', 'Alimento Gato Salmón 3kg', 'Pelota de Goma Resistente', 
+                'Hueso de Carnaza Mediano', 'Collar Antipulgas Gatos', 'Shampoo Hipoalergénico 500ml', 
+                'Pipeta Antiparasitaria Perros 10-20kg', 'Arena Aglutinante Lavanda', 'Rascador Torre 3 Niveles',
+                'Cama Acolchada L', 'Transportadora Rígida M', 'Juguete Interactivo Ratón',
+                'Cepillo Carda Suave', 'Cortaúñas Ergonómico', 'Suplemento Calcio Tabletas',
+                'Vitaminas Caninas Multi', 'Arnés Pechera Reflectivo', 'Correa Extensible 5m',
+                'Plato Acero Antideslizante', 'Fuente de Agua Automática', 'Bolsas Recogedoras Biodegradables',
+                'Pañales de Entrenamiento', 'Limpiador de Oídos', 'Pasta Dental Canina',
+                'Spray Calmante Feromonas', 'Jerky de Pollo Natural', 'Galletas Integrales Perro',
+                'Lata Paté Ternera', 'Sobres Salsa Pavo', 'Collar Isabelino 20cm',
+                'Bozal Nylon Talla M', 'Silbato Entrenamiento', 'Clicker Adiestramiento',
+                'Abrigo Impermeable Rojo', 'Suéter Lana Azul', 'Dispensador Comida Auto',
+                'Camita Cueva Gato', 'Tunel de Juego Plegable', 'Catnip en Spray'
+            ]),
             'descripcion' => $this->faker->sentence(),
             'marca' => $this->faker->company(),
             'precio_compra' => $precio * 0.7,
